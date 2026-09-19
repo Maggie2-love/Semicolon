5b73fd8c9d84c5bb74a2ac02452d31752c78ef7b
Maggie2-love <magdalenejohn260@gmail.com>
2026-09-19 22:43:26 +0100

    remove dupilcate files after organizing folders

--- Palindrome.py
+++
@@ -1,5 +0,0 @@
-word = input("Enter a 3 letter word :")
-if word[0]==word[2]:
-    print("Palindrome")
-else: 
-    print("Not a palindrome")
--- first_Character.py
+++
@@ -1,7 +0,0 @@
-text = input ("Enter a String: ")
-
-if text [0]. isupper(): 
-	print("Uppercase")
-else: 
-	print ("Lowercase")
-
