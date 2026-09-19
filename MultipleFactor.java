39ef74c435ad06f79788440e40a8dae586c44535
Maggie2-love <magdalenejohn260@gmail.com>
2026-09-19 21:39:20 +0100

    Add files via upload

---
+++ MultipleFactor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class MultipleFactor{ 
+public static void main (String[] args){
+
+    Scanner input=new Scanner(System.in);
+    System.out.print("Enter a number:");
+    int number=input.nextInt();
+    
+    
+    for(int index=1; index<=number; index++){
+  
+     if(number % index==0){
+    
+    System.out.println(index);
+      
+    }
+    
+    }
+    
+    }
+    
+   }
+   
+   
+   
+  
+   
